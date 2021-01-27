b0ff0b5d03e7f185ecdaabd0fb1db0db17b8314f
saebom <boma91@gmail.com>
2021-01-28 00:33:39 +0900

    Add: toDoList.js 파일 구현중 및 toDoList.scss 파일 작성 완료

--- src/pages/toDoList.js
+++ src/pages/toDoList.js
@@ -1,9 +1,39 @@
-import { render } from "@testing-library/react";
 import React, { Component } from "react";
+import "./toDoList.scss";
 
 class toDoList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            li: false,
+            checkBtn: false,
+            delBtn: false,
+        };
+    }
+
+    // state 객체의 list 생성하는 함수 >>> state 바꾸는 함수 (= setState)
+    handleList = () => {
+        // console.log("handleList 함수 연결 확인");
+        this.setState({
+            li: true,
+            checkBtn: true,
+            delBtn: true,
+        });
+    };
+
     render() {
-        return;
+        return (
+            <div className="toDoList">
+                <h1>To Do List</h1>
+                <div class="writeList">
+                    <input id="input" type="text" placeholder="Write your list to do..." />
+                    <button id="btn" onClick={this.handleList}>
+                        Add
+                    </button>
+                </div>
+                <ul class="addList"></ul>
+            </div>
+        );
     }
 }
 
